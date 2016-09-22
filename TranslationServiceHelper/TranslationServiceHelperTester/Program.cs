4618f2a6ca0c279b55df974b0bca732c54bfd1c8
Shaun Peoples <shaundg@users.noreply.github.com>
2016-09-22 02:19:40 -0700

    Add files via upload
    First and, likely only, commit.

---
+++ TranslationServiceHelper/TranslationServiceHelperTester/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TranslationServiceHelperTester.TerminologyService;
+
+
+namespace TranslationServiceHelperTester
+{
+    class Program
+    {
+        static TerminologyClient tc;
+        static TranslationSources ts;
+        static bool waitForResults = false;
+
+        static void Main(string[] args)
+        {
+            tc = new TerminologyClient();
+            ts = new TranslationSources() { TranslationSource.Terms, TranslationSource.UiStrings };
+
+            SearchStringComparison ssc = (args.Length > 3) && args[3] == "usecase" ? SearchStringComparison.CaseSensitive : SearchStringComparison.CaseInsensitive;
+
+            tc.GetTranslationsCompleted += tc_GetTranslationsCompleted;
+            tc.GetLanguagesCompleted += tc_GetLanguagesCompleted;
+
+            if (args.Length < 3)
+            {
+                tc.GetLanguagesAsync();
+                waitForResults = true;
+                while (waitForResults)
+                {
+                    Console.WriteLine("Waiting for languages retrieval results...\n");
+                }
+
+                string exeName = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+                Console.WriteLine("\n\n\n\nUsage: " + exeName + " \"phrase\"" + " \"source lang\"" + " \"dest lang\"" + " \"");
+
+                //Console.WriteLine("\n\n\n\nPress any key to exit...");
+                //Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Querying Translation Service...\n");
+
+            tc.GetTranslationsAsync(args[0],
+                        args[1],
+                        args[2],
+                        ssc,
+                        SearchOperator.Contains,
+                        ts,
+                        false,
+                        20,
+                        true,
+                        null);
+
+            waitForResults = true;
+            while (waitForResults)
+            {
+                Console.WriteLine("Waiting for translation results...\n");
+            }
+
+            //Console.WriteLine("\n\n\n\nPress any key to exit...\n");
+            //Console.ReadKey();
+        }
+
+        static void tc_GetLanguagesCompleted(object sender, GetLanguagesCompletedEventArgs e)
+        {
+            if (e.Error == null && e.Result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Got Languages Complete!\n");
+
+                Languages ls = e.Result;
+
+                string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string[] vals = path.Split('\\');
+                path = path.Remove(path.IndexOf(vals[vals.Length - 1]));
+                path += "lang.txt";
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
+                {
+                    foreach (Language l in ls)
+                    {
+                        file.WriteLine("{0}", l.Code);
+                    }
+                    waitForResults = false;
+                }
+                return;
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Failed On Translation Service!\n");
+                waitForResults = false;
+            }
+        }
+
+        static void tc_GetTranslationsCompleted(object sender, GetTranslationsCompletedEventArgs e)
+        {
+            if (e.Error == null && e.Result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Translation Complete!\n");
+                Matches m = e.Result;
+
+                string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+
+                string[] vals = path.Split('\\');
+                path = path.Remove(path.IndexOf(vals[vals.Length - 1]));
+                path += "trans.txt";
+
+
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(path))
+                {
+                    foreach (Match match in m)
+                    {
+                        file.Write("{0}"+ (char)15 + "{1}" + (char)15 + "{2}", match.OriginalText, match.Product + " " + match.ProductVersion, match.ConfidenceLevel);
+                        foreach (Translation t in match.Translations)
+                        {
+                            file.Write((char)14 + "{0}" + (char)15 +"{1}", t.TranslatedText, t.Language);
+                        }
+                        file.WriteLine("");
+                    }
+                }
+
+                waitForResults = false;
+                return;
+            }
+            else
+            {
+                Console.WriteLine("ERROR: Failed On Translation Service!\n");
+                waitForResults = false;
+            }
+        }
+    }
+}
---
+++ Unity Demo/Assets/TranslationWindow.cs
@@ -0,0 +1,217 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public class TranslationWindow : EditorWindow
+{
+	private Vector2 _scrollPos = Vector2.zero;
+	[MenuItem("Translationz/Get Translation", false, 10)]
+	static void Init()
+	{
+		GetWindow(typeof(TranslationWindow), true, "Translationz");
+	}
+
+	public static string[] languagesArray;
+	public List<string> Phrases;
+
+	int sourceLanguageIndex = -1;
+	int lastSourceLanguageIndex = -1;
+	int destLanguageIndex = -1;
+	int lastDestLanguageIndex = -1;
+	string phrase;
+
+	bool retrievingLanguages = false;
+	bool languagesPresent = false;
+	static bool languagesProcessed = false;
+
+	bool hasPhrases = false;
+	bool waitingOnTranslation = false;
+
+	const string transSourceLangIDkey = "translationz_source_lang_index";
+	const string transDestLangIDkey = "translationz_destination_lang_index";
+
+	void OnEnable()
+	{
+		sourceLanguageIndex = EditorPrefs.GetInt(transSourceLangIDkey, 0);
+		destLanguageIndex = EditorPrefs.GetInt(transDestLangIDkey, 0);
+
+		languagesPresent = LanguagesFileExists();
+		LanguageInitChecks();
+
+		//default settings
+		if (sourceLanguageIndex < 0 && destLanguageIndex < 0)
+		{
+			sourceLanguageIndex = System.Array.IndexOf(languagesArray, "en-us");
+			destLanguageIndex = System.Array.IndexOf(languagesArray, "es-es");
+			phrase = "seconds ago";
+		}
+	}
+
+	void OnDisable()
+	{
+		EditorPrefs.SetInt(transSourceLangIDkey, sourceLanguageIndex);
+		EditorPrefs.SetInt(transDestLangIDkey, destLanguageIndex);
+	}
+
+	void LanguageInitChecks()
+	{
+		if (!languagesPresent)
+		{
+			if (!retrievingLanguages)
+			{
+				retrievingLanguages = true;
+				string exe = Directory.GetCurrentDirectory();
+				exe += @"\Assets\Plugins\Translationz\GetTranslation.exe";
+				System.Diagnostics.Process external = new System.Diagnostics.Process();
+				external.StartInfo.FileName = exe;
+				external.StartInfo.Arguments = ""; //argument
+				external.StartInfo.UseShellExecute = false;
+				external.Start();
+			}
+			else
+			{
+				languagesPresent = LanguagesFileExists();
+				if (languagesPresent)
+					retrievingLanguages = false;
+			}
+		}
+		else if (!languagesProcessed)
+		{
+			string currentDir = Directory.GetCurrentDirectory();
+			currentDir += @"\Assets\Plugins\Translationz\lang.txt";
+			StreamReader input = new StreamReader(currentDir);
+
+			List<string> availableLanguages = new List<string>();
+			while (!input.EndOfStream)
+			{
+				string line = input.ReadLine();
+				availableLanguages.Add(line);
+			}
+
+			languagesArray = availableLanguages.ToArray();
+			languagesProcessed = true;
+
+			input.Close();
+		}
+	}
+
+	void OnGUI()
+	{
+		if (!languagesPresent || !languagesProcessed || retrievingLanguages)
+		{
+			GUILayout.BeginArea(new Rect(100, 100, 300, 300), "F");
+			GUILayout.Label("Getting Languages...");
+			GUILayout.EndArea();
+			LanguageInitChecks();
+			return;
+		}
+
+		EditorGUILayout.BeginVertical(EditorStyles.objectFieldThumb);
+		EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+		EditorGUILayout.LabelField("Source Language", GUILayout.MaxWidth(160));
+		lastSourceLanguageIndex = sourceLanguageIndex;
+		sourceLanguageIndex = EditorGUILayout.Popup(sourceLanguageIndex, languagesArray, GUILayout.MaxWidth(100));
+		if (lastSourceLanguageIndex != sourceLanguageIndex)
+		{
+			EditorPrefs.SetInt(transSourceLangIDkey, sourceLanguageIndex);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+		EditorGUILayout.LabelField("Destination Language", GUILayout.MaxWidth(160));
+		lastDestLanguageIndex = destLanguageIndex;
+		destLanguageIndex = EditorGUILayout.Popup(destLanguageIndex, languagesArray, GUILayout.MaxWidth(100));
+		if (lastDestLanguageIndex != destLanguageIndex)
+		{
+			EditorPrefs.SetInt(transDestLangIDkey, destLanguageIndex);
+		}
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+		phrase = EditorGUILayout.TextField(new GUIContent("Phrase", "Enter a phrase such as 'seconds ago' or 'Save to disk'"), phrase);
+		EditorGUILayout.EndHorizontal();
+
+		if (GUILayout.Button(new GUIContent("Go!", "This will retrieve translations for the phrase."), EditorStyles.toolbarButton, GUILayout.Width(160)))
+		{
+			hasPhrases = false;
+
+			string currentDir = Directory.GetCurrentDirectory();
+			currentDir += @"\Assets\Plugins\Translationz\trans.txt";
+			File.Delete(currentDir);
+
+			string argsVal = "\"" + phrase + "\" \"" + languagesArray[sourceLanguageIndex] + "\" \"" + languagesArray[destLanguageIndex] + "\"";
+			string exe = Directory.GetCurrentDirectory();
+			exe += @"\Assets\Plugins\Translationz\GetTranslation.exe";
+			System.Diagnostics.Process external = new System.Diagnostics.Process();
+			external.StartInfo.FileName = exe;
+			external.StartInfo.Arguments = argsVal; //argument 
+			external.StartInfo.UseShellExecute = false;
+			external.Start();
+			waitingOnTranslation = true;
+		}
+		EditorGUILayout.EndVertical();
+
+		if (waitingOnTranslation && !hasPhrases && PhrasesFileExists())
+		{
+			ReadTranslatedPhrase();
+		}
+
+		if (hasPhrases)
+		{
+			EditorGUILayout.LabelField("Phrases:", GUILayout.MaxWidth(160));
+			_scrollPos = EditorGUILayout.BeginScrollView(_scrollPos);
+
+			string val = "";
+			for (int i = 0; i < Phrases.Count; ++i)
+			{
+				string[] pieces = Phrases[i].Split((char)14);
+
+				string[] sourceAndProduct = pieces[0].Split((char)15);
+				string[] translationAndLanguage = pieces[1].Split((char)15);
+
+				val = sourceAndProduct[0] + "\t" + translationAndLanguage[0] + "\t\t\t" + sourceAndProduct[1] + " " + sourceAndProduct[2] + "\n";
+				EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+				EditorGUILayout.TextField(sourceAndProduct[0]);
+				EditorGUILayout.TextField(translationAndLanguage[0]);
+				EditorGUILayout.TextField(sourceAndProduct[1] + " " + sourceAndProduct[2]);
+
+				EditorGUILayout.EndHorizontal();
+			}
+			EditorGUILayout.EndScrollView();
+		}
+	}
+
+	bool LanguagesFileExists()
+	{
+		string currentDir = Directory.GetCurrentDirectory();
+		currentDir += @"\Assets\Plugins\Translationz\lang.txt";
+		return File.Exists(currentDir);
+	}
+
+	bool PhrasesFileExists()
+	{
+		string currentDir = Directory.GetCurrentDirectory();
+		currentDir += @"\Assets\Plugins\Translationz\trans.txt";
+		return File.Exists(currentDir);
+	}
+
+	void ReadTranslatedPhrase()
+	{
+		waitingOnTranslation = false;
+		string currentDir = Directory.GetCurrentDirectory();
+		currentDir += @"\Assets\Plugins\Translationz\trans.txt";
+		StreamReader input = new StreamReader(currentDir);
+		Phrases = new List<string>();
+
+		while (!input.EndOfStream)
+		{
+			string line = input.ReadLine();
+			Phrases.Add(line);
+		}
+
+		input.Close();
+		hasPhrases = true;
+	}
+}
